b65ef0eabcd3a7a5451032923854fe7eef7ee27e
Mikhail Shuvarov <shyvarov@gmail.com>
2020-07-22 16:27:14 +0300

    feat: add showPopupThank.js

--- src/modules/showPopupConsultation.js
+++ src/modules/showPopupConsultation.js
@@ -1,15 +1,15 @@
 const showPopupConsultation = () => {
-  const popupConsultation = document.querySelector('.popup-consultation');
+	const popupConsultation = document.querySelector('.popup-consultation');
 
-  document.body.addEventListener('click', event => {
-    if (event.target.matches('.button_wide')) {
-      popupConsultation.style.visibility = 'visible';
-    } else if (event.target.closest('.close.close-consultation') || !event.target.closest('.feedback-wrap')) {
-      popupConsultation.style.visibility = '';
+	document.body.addEventListener('click', event => {
+		if (event.target.matches('.button_wide')) {
+			popupConsultation.style.visibility = 'visible';
+		} else if (event.target.closest('.close.close-consultation') || !event.target.closest('.feedback-wrap')) {
+			popupConsultation.style.visibility = '';
 		}
-  });
+	});
 
-  document.body.addEventListener('keydown', event => {
+	document.body.addEventListener('keydown', event => {
 		if (event.key === 'Escape') {
 			popupConsultation.style.visibility = '';
 		}
