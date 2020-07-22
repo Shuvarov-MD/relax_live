4a85644865a75dc564353d7219fce732878a29ba
Mikhail Shuvarov <shyvarov@gmail.com>
2020-07-22 19:10:01 +0300

    fix: fixed maskPhone.js

--- src/modules/maskPhone.js
+++ src/modules/maskPhone.js
@@ -8,22 +8,18 @@ function maskPhone(selector, masked = '+7 (___) ___-__-__') {
 			val = this.value.replace(/\D/g, "");
 		console.log(template);
 		let i = 0,
-			newValue = template.replace(/[_\d]/g, function (a) {
-				return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-			});
+			newValue = template.replace(/[_\d]/g, a => (i < val.length ? val.charAt(i++) || def.charAt(i) : a));
 		i = newValue.indexOf("_");
-		if (i != -1) {
+		if (i !== -1) {
 			newValue = newValue.slice(0, i);
 		}
 		let reg = template.substr(0, this.value.length).replace(/_+/g,
-			function (a) {
-				return "\\d{1," + a.length + "}";
-			}).replace(/[+()]/g, "\\$&");
+			a => "\\d{1," + a.length + "}").replace(/[+()]/g, "\\$&");
 		reg = new RegExp("^" + reg + "$");
 		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
 			this.value = newValue;
 		}
-		if (event.type == "blur" && this.value.length < 5) {
+		if (event.type === "blur" && this.value.length < 5) {
 			this.value = "";
 		}
 
